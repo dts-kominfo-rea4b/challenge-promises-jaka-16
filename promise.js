2bdb9ec364a670de6c9b76ce967f5b8211a5d540
Jaka <pratama.jaka@gmail.com>
2022-09-18 22:31:24 +0700

    .

--- promise.js
+++ promise.js
@@ -1,8 +1,49 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let promiseOutput = null;
+  
+promiseOutput = (emosi) => {
+  return new Promise((resolved) => {
+    function countTheaterIXX(amount) { 
+          promiseTheaterIXX()
+              .then()
+                    .then((resolve) => {
+                        let count_marah = 0;
+                        let count_tdk_marah = 0;
+                        Object.values(resolve).forEach((value) => {
+                          let esmosi = value['hasil'];
+                          if (esmosi === 'marah'){
+                            count_marah+=4;
+                          }else{
+                            count_tdk_marah+=2;
+                          }
 
+                        })
+                        amount(emosi === 'marah' ? count_marah : count_tdk_marah);
+                      })
+                    }
+                      
+                    promiseTheaterVGC()
+                            .then()
+                                .then((resolve) => {
+                                    countTheaterIXX((x) => {
+                                        let count_marah = 0;
+                                        let count_tdk_marah = 0;
+                                        Object.values(resolve).forEach((value) => {
+                                            let esmosi = value['hasil'];
+                                                if (esmosi == 'marah'){
+                                                    count_marah+=4;
+                                                }else{
+                                                    count_tdk_marah+=2;
+                                                }
+
+                                        })
+                        return resolved(emosi === 'marah' ? `marah theaterIXX : ${count_marah}\nmarah theaterVGC : ${x}` : `tidak marah theaterIXX : ${count_tdk_marah}\ntidak marah theaterVGC : ${x}`)
+                                    })
+                                })
+                        })
+}
 module.exports = {
   promiseOutput,
 };
